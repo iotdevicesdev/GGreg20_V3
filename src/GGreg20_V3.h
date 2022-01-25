a6957672b6f3b522b9deb14b377b26598a07930d
IoT-devices LLC <96241971+iotdevicesdev@users.noreply.github.com>
2022-01-25 18:45:25 +0200

    Update GGreg20_V3.h

--- src/GGreg20_V3.h
+++ src/GGreg20_V3.h
@@ -22,16 +22,31 @@
   #include <WProgram.h>
 #endif
 
+/**************************************************************************/
+/*!
+    GGreg20_V3 Driver Library Constants Definition
+    May be tuned by the library user
+    GGREG20_V3_DEBOUNCE_TIME - a time for ISR "anti-bounce" protection on GPIO
+    GGREG20_V3_MEASURE_TIME - one pass measure time
+    GGREG20_V3_CONVFACTOR - SBM-20 GM-tube CPM to uSv/h conversion factor
+*/
+/**************************************************************************/
 #ifndef GGREG20_V3_DEBOUNCE_TIME
-#define GGREG20_V3_DEBOUNCE_TIME 5
+#define GGREG20_V3_DEBOUNCE_TIME 5 // milliseconds
 #endif
 #ifndef GGREG20_V3_MEASURE_TIME
-#define GGREG20_V3_MEASURE_TIME 60000
+#define GGREG20_V3_MEASURE_TIME 60000 // milliseconds
 #endif
 #ifndef GGREG20_V3_CONVFACTOR
 #define GGREG20_V3_CONVFACTOR 0.0054
 #endif
 
+/**************************************************************************/
+/*!
+    This class adds support for the GGreg20_V3 pulse output interface and handles it as 
+    an external interrupts on Arduino controller GPIO
+*/
+/**************************************************************************/
 class GGreg20_V3
 {
   public:
